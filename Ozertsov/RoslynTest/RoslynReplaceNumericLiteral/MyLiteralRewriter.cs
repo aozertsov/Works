57d9202f7a58cb4218cb5b251ecb63cb2b6207d9
m13oas <aozertsov@gmail.com>
2015-12-07 00:09:08 +0300

    First work with Roslyn

---
+++ Ozertsov/RoslynTest/RoslynReplaceNumericLiteral/MyLiteralRewriter.cs
@@ -0,0 +1,20 @@
+﻿namespace RoslynReplaceNumericLiteral {
+    using Roslyn.Compilers.CSharp;
+
+    class MyLiteralRewriter : SyntaxRewriter {
+        public override SyntaxNode VisitVariableDeclarator(VariableDeclaratorSyntax node) {
+            return node.Update(node.Identifier, node.ArgumentList, (EqualsValueClauseSyntax) this.VisitEqualsValueClause(node.Initializer));
+        }
+
+        public override SyntaxNode VisitEqualsValueClause(EqualsValueClauseSyntax node) {
+            SyntaxToken token = node.GetLastToken();
+            if(token.Kind == SyntaxKind.NumericLiteralToken) {
+                if(token.Value is int &&
+                    (int) token.Value == 11) {
+                    return node.ReplaceToken(token, Syntax.Literal(token.LeadingTrivia, "43", 43, token.TrailingTrivia));
+                }
+            }
+            return node;
+        }
+    }
+}
---
+++ Ozertsov/RoslynTest/RoslynReplaceNumericLiteral/SyntaxChanger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace RoslynReplaceNumericLiteral {
+    using Roslyn.Compilers;
+    using Roslyn.Compilers.CSharp;  
+
+    class SynraxChanger {
+        static void Main(string[] args) {
+            //TODO load from file
+            string text = @"using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 11
+            Console.WriteLine(a);
+        }
+    }
+}";
+            Console.WriteLine("Was");
+            Console.WriteLine(text);
+            SyntaxTree tree = SyntaxTree.ParseText(text);
+            CompilationUnitSyntax root = tree.GetRoot();
+            MyLiteralRewriter mlr = new MyLiteralRewriter();
+            //TODO more elegant search;
+            VariableDeclaratorSyntax childrens = (VariableDeclaratorSyntax) root.Members[0].ChildNodes().ElementAt(1).ChildNodes().ElementAt(0).ChildNodes().ElementAt(2).ChildNodes().ElementAt(0).ChildNodes().ElementAt(0).ChildNodes().ElementAt(1);
+            var a = mlr.VisitVariableDeclarator(childrens);
+            root = root.ReplaceNode(childrens, mlr.VisitVariableDeclarator(childrens));
+            tree = SyntaxTree.Create(root);
+            root = tree.GetRoot();
+            text = root.GetText().ToString();
+            //TODO update file
+            Console.WriteLine("After using Roslyn");
+            Console.WriteLine(text);
+        }
+    }
+}
